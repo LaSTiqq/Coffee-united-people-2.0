4608426539688367403e5c14779928051b5e29a0
Lauris Stirna <ls17118@edu.rta.lv>
2023-01-29 19:47:20 +0200

    Few minor readability fixes before implementing JWT Token

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import connectionToDb from "./utils/connectionToDb.js";
 import authRouter from "./routes/authRoute.js";
 import loggedInRouter from "./routes/loggedInRoute.js";
 
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.use("/", authRouter);
